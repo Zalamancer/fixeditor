a25af45ec0efbc6fb79f56a26e994f73e9b4be96
Ihsan Duru <duruihsan@gmail.com>
2025-08-31 04:17:22 +0000

    I dont see this document inside of supabase table? or as I even suppose

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -3,11 +3,36 @@ import "@blocknote/core/fonts/inter.css";
 import { useCreateBlockNote } from "@blocknote/react";
 import { BlockNoteView } from "@blocknote/mantine";
 import "@blocknote/mantine/style.css";
+import { createClient } from "@/lib/supabase/client";
+import { SupabaseProvider } from "y-supabase";
+import * as Y from "yjs";
+
+const supabase = createClient();
+const doc = new Y.Doc();
+
+// A basic Supabase real-time provider
+const provider = new SupabaseProvider(doc, supabase, {
+  channel: "documents",
+  id: "document-1",
+  tableName: "documents",
+  columnName: "data",
+});
 
 // Our <Editor> component we can reuse later
 export default function Editor() {
   // Creates a new editor instance.
-  const editor = useCreateBlockNote();
+  const editor = useCreateBlockNote({
+    collaboration: {
+      provider,
+      // Where to store BlockNote data in the Y.Doc:
+      fragment: doc.getXmlFragment("document-xml"),
+      // Information for this user:
+      user: {
+        name: "My User",
+        color: "#ff0000",
+      },
+    },
+  });
 
   // Renders the editor instance using a React component.
   return <BlockNoteView editor={editor} />;
